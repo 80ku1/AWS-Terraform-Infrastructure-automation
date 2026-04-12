3b77b4081eee3533ca9930f09461b2916d80f2a9
Goku <gokulgokuz21@gmail.com>
2026-04-12 13:08:39 +0530

    modified security group

--- security.tf
+++ security.tf
@@ -8,18 +8,25 @@ resource "aws_security_group" "mySecurityGroup" {
   description = "security_group_for_dev"
   vpc_id      = aws_vpc.myVPC.id
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.my_ip.response_body)}/32"]
-
+  tags = {
+    Name = "mySecurityGroup"
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
+# Allow SSH only from current public IP
+resource "aws_vpc_security_group_ingress_rule" "allow_traffic_ipv4_ssh" {
+  security_group_id = aws_security_group.mySecurityGroup.id
+  cidr_ipv4         = "${chomp(data.http.my_ip.response_body)}/32"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+# Allow all outbound traffic
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.mySecurityGroup.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
+
